0f29dc04b35fba0b361dc56a9acaf6118237b211
Lena Kozlova <lenaklenina1991@gmail.com>
2020-10-21 16:42:04 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let remainingStack = str.split('').reduce((stack, current, index) => {
+	  const isOpening = bracketsConfig.filter((conf) => { return conf[0] == current; });
+	  const isClosing = bracketsConfig.filter((conf) => { return conf[1] == current; });
+	 
+	  if (index == 0) {
+		  stack.push(current);
+	  } else {	  
+		  if ((isClosing[0] && isClosing[0][0] != isClosing[0][1]) || (isOpening[0] && isOpening[0][0] != isOpening[0][1])) {
+		  if (isOpening.length > 0 || stack.length == 0) {
+			  stack.push(current);
+		  } else if (isClosing.length > 0 && stack[stack.length - 1] == isClosing[0][0]) {
+			  stack.pop();
+		  }
+		} else if (stack[stack.length - 1] != isClosing[0][0]) {
+			 stack.push(current);
+		} else {
+			stack.pop();
+		}
+	  }  
+	  
+	  
+	  return stack;
+  }, []);
+  
+  return remainingStack.length == 0 ? remainingStack.length == 0 : remainingStack.length == 0;
 }
